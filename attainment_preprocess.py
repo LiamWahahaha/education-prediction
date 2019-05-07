97b04b33c4f56884dea38bbaaa6e1dcaed8cc977
Liam <liliwang@cs.stonybrook.edu>
2019-05-07 12:54:45 -0400

    Add ignore file and several unzip json files
    attainment_preprocess.py transfers education_attainment.xls into
    education_attainment.csv, which requires further works to do,
    such as lower case for all the state and area features, and
    remove the 'county' from area feature.
    
    gather_twitter_file.py will create a file - twitter_file_list.txt,
    this file will be used in the spark script.

---
+++ attainment_preprocess.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+cols = [
+    'FIPS Code', 'State', 'Area name',
+    'Percent of adults with less than a high school diploma, 1980',
+    'Percent of adults with a high school diploma only, 1980',
+    'Percent of adults completing some college (1-3 years), 1980',
+    'Percent of adults completing four years of college or higher, 1980',
+    'Percent of adults with less than a high school diploma, 1990',
+    'Percent of adults with a high school diploma only, 1990',
+    'Percent of adults completing some college or associate\'s degree, 1990',
+    'Percent of adults with a bachelor\'s degree or higher, 1990',
+    'Percent of adults with less than a high school diploma, 2000',
+    'Percent of adults with a high school diploma only, 2000',
+    'Percent of adults completing some college or associate\'s degree, 2000',
+    'Percent of adults with a bachelor\'s degree or higher, 2000',
+    'Percent of adults with less than a high school diploma, 2013-17',
+    'Percent of adults with a high school diploma only, 2013-17',
+    'Percent of adults completing some college or associate\'s degree, 2013-17',
+    'Percent of adults with a bachelor\'s degree or higher, 2013-17'
+]
+
+attainment = pd.read_excel('education_attainment.xls', skiprows=4, usecols=cols)
+attainment.rename(
+    columns={
+        'FIPS Code':
+        'FIPS',
+        'Area name':
+        'Area',
+        'Percent of adults with less than a high school diploma, 1980':
+        '1980-0',
+        'Percent of adults with a high school diploma only, 1980':
+        '1980-1',
+        'Percent of adults completing some college (1-3 years), 1980':
+        '1980-2',
+        'Percent of adults completing four years of college or higher, 1980':
+        '1980-3',
+        'Percent of adults with less than a high school diploma, 1990':
+        '1990-0',
+        'Percent of adults with a high school diploma only, 1990':
+        '1990-1',
+        'Percent of adults completing some college or associate\'s degree, 1990':
+        '1990-2',
+        'Percent of adults with a bachelor\'s degree or higher, 1990':
+        '1990-3',
+        'Percent of adults with less than a high school diploma, 2000':
+        '2000-0',
+        'Percent of adults with a high school diploma only, 2000':
+        '2000-1',
+        'Percent of adults completing some college or associate\'s degree, 2000':
+        '2000-2',
+        'Percent of adults with a bachelor\'s degree or higher, 2000':
+        '2000-3',
+        'Percent of adults with less than a high school diploma, 2013-17':
+        '2013-0',
+        'Percent of adults with a high school diploma only, 2013-17':
+        '2013-1',
+        'Percent of adults completing some college or associate\'s degree, 2013-17':
+        '2013-2',
+        'Percent of adults with a bachelor\'s degree or higher, 2013-17':
+        '2013-3'
+    },
+    inplace=True)
+
+attainment.to_csv(path_or_buf='education_attainment.csv', index=False)
---
+++ gather_twitter_file.py
@@ -0,0 +1,42 @@
+import os
+'''
+    For the given path, get the List of all json files in the directory tree
+'''
+
+
+def get_list_of_files(dir_name):
+    # create a list of file and sub directories
+    # names in the given directory
+    entries = os.listdir(dir_name)
+    files = list()
+
+    # Iterate over all the entries
+    for entry in entries:
+        full_path = os.path.join(dir_name, entry)
+        if os.path.isdir(full_path):
+            files = files + getListOfFiles(full_path)
+        else:
+            files.append(full_path)
+
+    return files
+
+
+def main():
+    dir_name = 'twitter'
+
+    # Get the list of all files in directory tree at given path
+    files = list()
+    for (dir_path, dir_names, file_names) in os.walk(dir_name):
+        files += [
+            os.path.join(dir_path, file) for file in file_names
+            if file[-5:] == '.json'
+        ]
+
+    # Write the list of json files to twitter_file_list.txt
+    f = open("twitter/twitter_file_list.txt", "w")
+    f.write(','.join(files))
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
