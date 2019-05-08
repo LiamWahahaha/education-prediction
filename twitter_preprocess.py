17dab4ab4a928a2591550f7c8f421fc2b3ca95e8
Liam <liliwang@cs.stonybrook.edu>
2019-05-08 09:10:24 -0400

    run this script will create a hdfs like file, make sure modify the default file name

--- twitter_preprocess.py
+++ twitter_preprocess.py
@@ -3,7 +3,9 @@ import csv
 from pprint import pprint
 import json
 from pyspark import SparkContext
+from geopy.geocoders import Nominatim
 
+geolocator = Nominatim(user_agent="specify_your_app_name_here")
 sc = SparkContext()
 sc.setLogLevel("ERROR")
 f = open('twitter/twitter_file_list.txt')
@@ -56,32 +58,27 @@ def parse_location(location):
     location_info = location.lower().split(',')
     county = location_info[0]
     state = None
-
     if len(location_info) > 1:
         state = location_info[1].replace(' ', '')
-
     return state, county
 
 
 def remove_trailing_chars(string):
     last_idx = len(string) - 1
     last_char = string[last_idx]
-
     while last_idx and string[last_idx - 1] == last_char:
         last_idx -= 1
-
     return string[:last_idx + 1]
 
 
 def add_state_county_features(line):
     # assume that if user provide county info, it will be only 'county', or 'county, states'
-    line['state'], line['county'] = parse_location(line['location'])
-
+    state, county = parse_location(line['location'])
     return ({
         'user_id': line['user_id'],
         'location': line['location'],
-        'state': line['state'],
-        'county': line['county'],
+        'state': state,
+        'county': county,
         'time_zone': line['time_zone'],
         'text': line['text'],
         'created_at': line['created_at']
@@ -90,4 +87,57 @@ def add_state_county_features(line):
 
 twitter_w_proper_area = twitter_en_user_w_loc.map(add_state_county_features)
 
-pprint(twitter_w_proper_area.take(5))
+
+def geo_location(line):
+    location = line['location'].split(',')
+    try:
+        latitude = float(location[0])
+        longitude = float(location[1])
+        address = geolocator.reverse('{},{}'.format(latitude,
+                                                    longitude)).address
+        if address[-3:] == 'USA':
+            return (int(address.split(',')[-2]),
+                    (address, line['user_id'], line['text'],
+                     line['created_at']))
+        else:
+            return (None, None)
+    except (KeyError, ValueError, IndexError):
+        latitude = None
+        longitude = None
+        return (None, None)
+    except:
+        # query time out
+        return (None, None)
+
+
+# twitter_parsed_by_geo = twitter_w_proper_area.filter(lambda line: line['state'] == None).map(geo_location)
+twitter_parsed_by_geo = twitter_w_proper_area.map(
+    geo_location).filter(lambda line: line[0] is not None)
+
+raw_zipcode2fips_data = sc.textFile(
+    'ZIP-COUNTY-FIPS_2018-03.csv').mapPartitions(lambda line: csv.reader(line))
+zipcode2fips_header = raw_zipcode2fips_data.first()
+zipcode2fips_data = raw_zipcode2fips_data.filter(
+    lambda line: line != zipcode2fips_header).map(lambda line: (int(line[0]),
+                                                                int(line[1])))
+
+intermediate = twitter_parsed_by_geo.join(zipcode2fips_data)
+
+
+def transfer2intermediate(line):
+    ((address, user_id, text, created_at), fips) = line[1]
+    return fips, user_id, text, created_at
+
+
+def to_csv(line):
+    return ','.join(str(data) for data in line)
+
+
+lines = intermediate.map(transfer2intermediate).map(to_csv)
+lines.saveAsTextFile('./twitter/2011-09-intermediate.csv')
+print('****************************')
+print('****************************')
+print('total records:', raw_twitter_data.count())
+print('valid records:', intermediate.count())
+print('****************************')
+print('****************************')
