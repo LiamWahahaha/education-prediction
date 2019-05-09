a8f2409e905c9593eae7a4c37bbcd5e21ae12ae7
Liam <liliwang@cs.stonybrook.edu>
2019-05-09 19:32:04 -0400

    Remove url and emoji from tweet_body, add url_count and emoji_count features

--- tweet_parser.py
+++ tweet_parser.py
@@ -6,41 +6,6 @@ from pyspark import SparkContext, SparkConf
 from tweet_utils import *
 from education_location_utils import *
 
-def aggregate_county_data(county_tweets_info, fips_dict):
-    fip = county_tweets_info[0]
-    education_info = fips_dict[fip]
-
-    county_tweets = county_tweets_info[1]
-
-    aggregate_record = {}
-    aggregate_record['fip'] = fip
-    
-    words_dict = {}
-    tweet_count = 0
-
-    for tweet_info in county_tweets:
-        tweet_count += 1
-        tweet_body = tweet_info['text']
-        tweet_words = tweet_body.split()
-        
-        for tweet_word in tweet_words:
-            if tweet_word not in words_dict:
-                words_dict[tweet_word] = 1
-            words_dict[tweet_word] += 1
-
-    total_word_freq = sum(words_dict.values())
-
-    for word in words_dict:
-        words_dict[word] /= total_word_freq
-
-
-    aggregate_record['words'] = words_dict
-    aggregate_record['tweet_count'] = tweet_count
-    aggregate_record['education_info'] = education_info[-4:]
-
-    return aggregate_record
-
-
 if __name__ == "__main__":
     sc = SparkContext(conf=SparkConf())
     sc.setLogLevel("ERROR")
@@ -54,12 +19,10 @@ if __name__ == "__main__":
     raw_twitter_json = raw_twitter_data.map(json.loads)
 
     # pprint(raw_twitter_json)
+    # TODO: describe the process
     filtered_twitter = raw_twitter_json.map(filter_twitter_raw_data)
-
     twitter_en_user_w_loc = filtered_twitter.filter(english_user_with_location)
-
     twitter_w_proper_area = twitter_en_user_w_loc.map(add_state_county_features)
-
 
     #get all the data for education stats; mapped by FIPS and County name
     attainment_data_rdd = sc.textFile(attainment_data).mapPartitions(lambda line: csv.reader(line))
@@ -67,12 +30,11 @@ if __name__ == "__main__":
     attainment_data_rdd = attainment_data_rdd.filter(lambda line: line != attainment_data_header)
     attainment_data_rdd = sc.broadcast(attainment_data_rdd.collect())
     attainment_data_rdd_values = attainment_data_rdd.value
+
     #get info by FIPS and county name
     fips_dict, county_dict = create_education_dicts(attainment_data_rdd_values)
     #print(county_dict)
-    
     #pprint(fips_dict)
-
 
     #map zipcodes to fips
     zipcode_data_rdd = sc.textFile(zipcode_data).mapPartitions(lambda line: csv.reader(line))
@@ -80,22 +42,22 @@ if __name__ == "__main__":
     zipcode_data_rdd = zipcode_data_rdd.filter(lambda line: line != zipcode_data_header)
     zipcode_data_rdd = sc.broadcast(zipcode_data_rdd.collect())
     zipcode_data_rdd_values = zipcode_data_rdd.value
+
     #get info by zipcode
     zipcode_dict = create_zipcode_dict(zipcode_data_rdd_values)
     #pprint(zipcode_dict)
 
-    
-    twitter_w_proper_area = twitter_w_proper_area.map(lambda x : map_tweet_to_location(x, fips_dict, county_dict, zipcode_dict))
+    twitter_w_proper_area = twitter_w_proper_area.map(lambda x: map_tweet_to_location(x, fips_dict, county_dict, zipcode_dict))
     # pprint(twitter_w_proper_area.take(1))
     # pprint(twitter_w_proper_area.count())
 
-    tweets_w_fips = twitter_w_proper_area.filter(lambda x : x['fips'] != None)
+    tweets_w_fips = twitter_w_proper_area.filter(lambda x: x['fips'] != None)
     #tweets_w_fips = tweets_w_fips.groupBy(lambda x : x['fips'])
-    tweets_w_fips = tweets_w_fips.groupBy(lambda x : x['fips']).mapValues(list)
+    tweets_w_fips = tweets_w_fips.groupBy(lambda x: x['fips']).mapValues(list)
     pprint(tweets_w_fips.take(25))
     pprint(tweets_w_fips.count())
 
     #aggregate info for each county (words used, education levels, etc)
-    county_level_info = tweets_w_fips.map(lambda x : aggregate_county_data(x, fips_dict))
+    county_level_info = tweets_w_fips.map(lambda x: aggregate_county_data(x, fips_dict))
     pprint(county_level_info.take(25))
     pprint(county_level_info.count())
--- tweet_utils.py
+++ tweet_utils.py
@@ -5,128 +5,189 @@ nltk.download('stopwords')
 #nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from collections import defaultdict
+from emoji import UNICODE_EMOJI
+import re
+
 
 def remove_trailing_chars(string):
-	if not string:
-		return string
-	last_idx = len(string) - 1
-	last_char = string[last_idx]
+    if not string:
+        return string
+    last_idx = len(string) - 1
+    last_char = string[last_idx]
 
-	while last_idx and string[last_idx - 1] == last_char:
-		last_idx -= 1
+    while last_idx and string[last_idx - 1] == last_char:
+        last_idx -= 1
 
-	return string[:last_idx + 1]
+    return string[:last_idx + 1]
+
+
 '''
 For a given tweet, tries to map the tweet to a known location
 If it's from a know location, the tweet is assigned the FIP and the body (text) is cleaned for processing
 '''
 def map_tweet_to_location(tweet_info, fips_dict, county_dict, zipcode_dict):
-	county = tweet_info['county']
-	state = tweet_info['state']
-	coordinates = tweet_info['coordinates']
-	#tweet_info['education_stats'] = None
-	tweet_info['fips'] = None
+    county = tweet_info['county']
+    state = tweet_info['state']
+    coordinates = tweet_info['coordinates']
+    #tweet_info['education_stats'] = None
+    tweet_info['fips'] = None
 
-	if county != None and county in county_dict:
-		#tweet_info['education_stats'] = county_dict[county]
-		tweet_info['fips'] = fip = county_dict[county][0]
-	elif state != None and state in county_dict:
-		#tweet_info['education_stats'] = county_dict[state]
-		tweet_info['fips'] = fip = county_dict[state][0]
-	elif coordinates != None and len(coordinates) != 0:
-		lat = coordinates[0]
-		long = coordinates[1]
-		zipcode_search = SearchEngine(simple_zipcode=False)
-		result = zipcode_search.by_coordinates(lat, long, radius=10, returns=1)
-		if len(result) != 0:
-			place = result[0]
-			fip = zipcode_dict[place.zipcode]
-			if fip in fips_dict:
-				#tweet_info['fips'] = fip
-				tweet_info['education_stats'] = fips_dict[fip]
+    if county != None and county in county_dict:
+        #tweet_info['education_stats'] = county_dict[county]
+        tweet_info['fips'] = fip = county_dict[county][0]
+    elif state != None and state in county_dict:
+        #tweet_info['education_stats'] = county_dict[state]
+        tweet_info['fips'] = fip = county_dict[state][0]
+    elif coordinates != None and len(coordinates) != 0:
+        lat = coordinates[0]
+        long = coordinates[1]
+        zipcode_search = SearchEngine(simple_zipcode=False)
+        result = zipcode_search.by_coordinates(lat, long, radius=10, returns=1)
+        if len(result) != 0:
+            place = result[0]
+            fip = zipcode_dict[place.zipcode]
+            if fip in fips_dict:
+                #tweet_info['fips'] = fip
+                tweet_info['education_stats'] = fips_dict[fip]
 
-	#clean the tweet body by getting rid of stop words
-	if tweet_info['fips'] != None:
-		tweet_body = tweet_info['text'] 
-		tweet_info['tweet_length_unfiltered'] = len(tweet_body)
-		stop_words = set(stopwords.words('english'))
-		stop_words.add(' ')
+    #clean the tweet body by getting rid of stop words
+    if tweet_info['fips'] != None:
+        tweet_body = tweet_info['text']
+        tweet_info['tweet_length_unfiltered'] = len(tweet_body)
+        stop_words = set(stopwords.words('english'))
+        stop_words.add(' ')
 
-		tweet_body_tokens = tweet_body.split()
-		filtered_sentence = [w for w in tweet_body_tokens if not w in stop_words] 
+        tweet_body_tokens = tweet_body.split()
+        filtered_sentence = [
+            w for w in tweet_body_tokens if not w in stop_words
+        ]
 
-		tweet_info['text'] = ' '.join(filtered_sentence)
+        tweet_info['text'] = ' '.join(filtered_sentence)
 
+    return tweet_info
 
-
-	return tweet_info
 
 def parse_location(location):
-	if not location:
-		return None, None
-	
-	location_info = location.lower().split(',')
-	county = remove_trailing_chars(location_info[0])
-	county = county.replace('County', '').replace('Borough', '').replace('Municipio', '')\
-		.replace('Municipality', '').replace('Census', '').replace('Area', '')\
-		.lower().replace(' ', '')
-	state = None
+    if not location:
+        return None, None
 
-	if len(location_info) > 1:
-		state = location_info[1].replace(' ', '')
+    location_info = location.lower().split(',')
+    county = remove_trailing_chars(location_info[0])
+    county = county.replace('County', '').replace('Borough', '').replace('Municipio', '')\
+     .replace('Municipality', '').replace('Census', '').replace('Area', '')\
+     .lower().replace(' ', '')
+    state = None
 
-	if state:
-		state = remove_trailing_chars(state)
+    if len(location_info) > 1:
+        state = location_info[1].replace(' ', '')
 
-	return state, county
+    if state:
+        state = remove_trailing_chars(state)
+
+    return state, county
+
 
 def parse_geo(geo):
-	if not geo or not geo['coordinates']:
-		return None
-	return geo['coordinates']
+    if not geo or not geo['coordinates']:
+        return None
+    return geo['coordinates']
 
 
 def add_state_county_features(line):
-	# assume that if user provide county info, it will be only 'county', or 'county, states'
-	line['state'], line['county'] = parse_location(line['location'])
-	line['coordinates'] = parse_geo(line['geo'])
+    # assume that if user provide county info, it will be only 'county', or 'county, states'
+    line['state'], line['county'] = parse_location(line['location'])
+    line['coordinates'] = parse_geo(line['geo'])
 
-	return ({
-		'user_id': line['user_id'],
-		'location': line['location'],
-		'state': line['state'],
-		'county': line['county'],
-		'time_zone': line['time_zone'],
-		'text': line['text'],
-		'created_at': line['created_at'],
-		'coordinates': line['coordinates']
-	})
+    return ({
+        'user_id': line['user_id'],
+        'location': line['location'],
+        'state': line['state'],
+        'county': line['county'],
+        'time_zone': line['time_zone'],
+        'text': line['text'],
+        'created_at': line['created_at'],
+        'coordinates': line['coordinates']
+    })
+
 
 def filter_twitter_raw_data(line):
-	try:
-		return ({
-			'user_id': line['user']['id'],
-			'lang': line['user']['lang'],
-			'location': line['user']['location'],
-			'state': None,
-			'county': None,
-			'time_zone': line['user']['time_zone'],
-			'text': line['text'],
-			'created_at': line['created_at'],
-			'geo': line['geo']
-		})
-	except (KeyError, AttributeError):
-		return ({
-			'user_id': None,
-			'lang': None,
-			'location': None,
-			'state': None,
-			'county': None,
-			'time_zone': None,
-			'text': None,
-			'created_at': None,
-			'geo': None
-		})
+    try:
+        return ({
+            'user_id': line['user']['id'],
+            'lang': line['user']['lang'],
+            'location': line['user']['location'],
+            'state': None,
+            'county': None,
+            'time_zone': line['user']['time_zone'],
+            'text': line['text'],
+            'created_at': line['created_at'],
+            'geo': line['geo']
+        })
+    except (KeyError, AttributeError):
+        return ({
+            'user_id': None,
+            'lang': None,
+            'location': None,
+            'state': None,
+            'county': None,
+            'time_zone': None,
+            'text': None,
+            'created_at': None,
+            'geo': None
+        })
+
 
 def english_user_with_location(line):
-	return line['user_id'] != None and (line['location'] != None or line['geo'] != None) and line['lang'] == 'en'
+    return line['user_id'] != None and\
+           line['lang'] == 'en' and\
+           (line['location'] != None or line['geo']!= None)
+
+
+def is_url(text):
+    return re.search('https?://+', text) is not None
+
+
+def is_emoji(text):
+    return any(map(lambda char: char in UNICODE_EMOJI, text))
+
+
+def aggregate_county_data(county_tweets_info, fips_dict):
+    fip = county_tweets_info[0]
+    education_info = fips_dict[fip]
+
+    county_tweets = county_tweets_info[1]
+
+    aggregate_record = dict()
+    aggregate_record['fip'] = fip
+
+    words_dict = defaultdict(float)
+    tweet_count = 0
+    emoji_count = 0
+    url_count = 0
+
+    for tweet_info in county_tweets:
+        tweet_count += 1
+        tweet_body = tweet_info['text']
+        tweet_words = tweet_body.split()
+
+        for tweet_word in tweet_words:
+            if is_url(tweet_word):
+                url_count += 1
+            elif is_emoji(tweet_word):
+                emoji_count += 1
+            else:
+                words_dict[tweet_word] += 1
+
+    total_word_freq = sum(words_dict.values())
+
+    for word in words_dict:
+        words_dict[word] /= total_word_freq
+
+    aggregate_record['words'] = words_dict
+    aggregate_record['tweet_count'] = tweet_count
+    aggregate_record['url_count'] = url_count
+    aggregate_record['emoji_count'] = emoji_count
+    aggregate_record['education_info'] = education_info[-4:]
+
+    return aggregate_record
